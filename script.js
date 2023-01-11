3cbda79d4fbe463c47384d30bb0b0b12103dbe34
Diogo Pereira <90282042+diogopdev@users.noreply.github.com>
2023-01-11 03:06:45 +0000

    Created functions to calculate
    +sum
    +subtract
    +divide
    +multiply
    +check operator
    +total
    +insert number
    +delete number

--- script.js
+++ script.js
@@ -1,10 +1,78 @@
 const displayTop = document.getElementById('displayTop');
 const displayBottom = document.getElementById('displayBottom');
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator;
 
-function insert (value) {
-	return displayBottom.innerHTML = displayBottom.innerHTML + value;
+function sum (number1, number2){
+	return number1 + number2;
 }
 
+function subtract(number1, number2){
+	return number1 - number2;
+}
+
+function divide(number1, number2){
+	return number1 / number2;
+}
+
+function multiply(number1, number2){
+	return number1 * number2;
+}
+
+function operate (operator, firstOperand, secondOperand){
+	switch (operator) {
+		case '+':
+			console.log('Entrei no +');
+		  return sum(firstOperand, secondOperand);
+		case '-':
+			console.log('Entrei no -');
+		  return subtract(firstOperand, secondOperand)
+		case 'x':
+			console.log('Entrei no *');
+		  return multiply(firstOperand, secondOperand)
+		case '/':
+			console.log('Entrei no /');
+		  if (secondOperand === 0){
+			return null
+		} else {
+			return divide(firstOperand, secondOperand)
+		}
+		default:
+			console.log('Não entrei');
+		  return null
+		}
+}
+
+function insertOperator (operator) {
+	if (displayTop != ''){
+		console.log('entrei')
+		firstOperand = Number(displayTop.textContent);
+	}
+	firstOperand = Number(displayBottom.textContent);
+	displayBottom.textContent = '';
+	currentOperator = operator;
+	displayTop.textContent = firstOperand + ' ' + currentOperator;
+	console.log(firstOperand, currentOperator);
+}
+
+function total (){
+	secondOperand = Number(displayBottom.textContent);
+	displayTop.textContent = displayTop.textContent + ' ' + secondOperand + ' =';
+	displayBottom.textContent = (operate(currentOperator, firstOperand, secondOperand));
+}
+
+function insert (number) {
+	return displayBottom.textContent = displayBottom.textContent + number;
+}
+
+function deleteNumber() {
+	displayBottom.textContent = displayBottom.textContent
+	  .toString()
+	  .slice(0, -1)
+  }
+
 function clearAll(){
-	return displayBottom.innerHTML = '';
+	displayTop.textContent = '';
+	displayBottom.textContent = '';
 }
